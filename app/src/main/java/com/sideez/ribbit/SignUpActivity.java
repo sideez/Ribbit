6c39afca0a9aebb0940e3ab081e8c3272340cbfa
Faisal Ali <siddee.faisalali@gmail.com>
2015-09-28 16:10:51 -0400

    creating and saving new user to Parse.com

--- app/src/main/java/com/sideez/ribbit/SignUpActivity.java
+++ app/src/main/java/com/sideez/ribbit/SignUpActivity.java
@@ -1,11 +1,16 @@
 package com.sideez.ribbit;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -32,6 +37,30 @@ public class SignUpActivity extends AppCompatActivity {
 
         } else {
             // this is where we will save user
+            ParseUser newUser = new ParseUser();
+            newUser.setUsername(username);
+            newUser.setPassword(password);
+            newUser.setEmail(email);
+            newUser.signUpInBackground(new SignUpCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e == null) {
+                        // Success! Parse auto authenticate user, send user to inbox (MainActivity)
+                        Intent intent = new Intent(SignUpActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                    } else {
+                        // Sign up didn't succeed.
+                        AlertDialog.Builder builder = new AlertDialog.Builder(SignUpActivity.this);
+                        builder.setMessage(e.getMessage())
+                                .setTitle(R.string.signup_error_title)
+                                .setPositiveButton(android.R.string.ok, null);
+                        AlertDialog dialog = builder.create();
+                        dialog.show();
+                    }
+                }
+            });
         }
     }
 
