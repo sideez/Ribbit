287ecc31a00b80790f134005d9a57b6eaa02d453
Faisal Ali <siddee.faisalali@gmail.com>
2015-09-29 11:46:36 -0400

    added the Edit Friends menu and screen

--- app/src/main/java/com/sideez/ribbit/MainActivity.java
+++ app/src/main/java/com/sideez/ribbit/MainActivity.java
@@ -113,6 +113,9 @@ public class MainActivity extends AppCompatActivity implements ActionBar.TabList
             if (currentUser == null) {
                 navigateToLogin();
             }
+        } else if (id == R.id.action_edit_friends) {
+            Intent intent = new Intent(this, EditFriendsActivity.class);
+            startActivity(intent);
         }
 
         return super.onOptionsItemSelected(item);
