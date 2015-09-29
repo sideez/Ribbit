e899330cd656dbe6dda45deec061a9d96e865cb5
Faisal Ali <siddee.faisalali@gmail.com>
2015-09-29 13:28:24 -0400

    setting up and executing query for users

--- app/src/main/java/com/sideez/ribbit/EditFriendsActivity.java
+++ app/src/main/java/com/sideez/ribbit/EditFriendsActivity.java
@@ -1,16 +1,70 @@
 package com.sideez.ribbit;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.widget.ArrayAdapter;
 
-public class EditFriendsActivity extends AppCompatActivity {
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.List;
+
+public class EditFriendsActivity extends ListActivity {
+
+    private static final String TAG = EditFriendsActivity.class.getSimpleName();
+
+    private List<ParseUser> mUsers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_friends);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        setProgressBarIndeterminate(true);
+
+        ParseQuery<ParseUser> parseQuery = ParseUser.getQuery();
+        parseQuery.orderByAscending(ParseConstants.KEY_USERNAME);
+        parseQuery.setLimit(1000);
+        parseQuery.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> list, ParseException e) {
+                setProgressBarIndeterminate(false);
+                if (e == null) {
+                    mUsers = list;
+                    String[] usernames = new String[mUsers.size()];
+                    int i = 0;
+                    for (ParseUser user : mUsers) {
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(EditFriendsActivity.this,
+                            android.R.layout.simple_list_item_checked, usernames);
+                    setListAdapter(adapter);
+                } else {
+                    Log.e(TAG, e.getMessage());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(EditFriendsActivity.this);
+                    builder.setMessage(e.getMessage())
+                            .setTitle(R.string.generic_error_title)
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
+
     }
 
     @Override
---
+++ app/src/main/java/com/sideez/ribbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.sideez.ribbit;
+
+/**
+ * Created by sideez on 2015-09-29.
+ */
+public final class ParseConstants {
+    // Class Names
+
+    // Field Names
+
+    public static final String KEY_USERNAME = "username";
+}
